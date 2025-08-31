267027e9f3f97a3b6f1a481c183e5af487b33ef5
Rob Bertram <rbertram8@gmail.com>
2025-08-31 01:39:27 -0400

    add misc plugins

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,8 +1,32 @@
 return {
     {
         "neovim/nvim-lspconfig",
+        dependencies = { "hrsh7th/cmp-nvim-lsp" },
         config = function()
+            local lspconfig = require("lspconfig")
+            local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
+            local on_attach = function(client, bufnr)
+                local opts = { noremap = true, silent = true, buffer = bufnr }
+
+                vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
+                vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+                vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+                vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
+                vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, opts)
+                vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
+                vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
+                vim.keymap.set("n", "<leader>fm", function() vim.lsp.buf.format { async = true } end, opts)
+            end
+
+            local servers = require("mason-lspconfig").get_installed_servers()
+
+            for _, server_name in ipairs(servers) do
+                lspconfig[server_name].setup {
+                    on_attach = on_attach,
+                    capabilities = capabilities,
+                }
+            end
         end
     }
 }
--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -2,5 +2,14 @@ return {
     {
         "mason-org/mason.nvim",
         opts = {}
+    },
+    {
+        "williamboman/mason-lspconfig.nvim",
+        dependencies = { "neovim/nvim-lspconfig", "mason-org/mason.nvim" },
+        opts = {
+            ensure_installed = {
+                "ts_ls"
+            }
+        }
     }
 }
