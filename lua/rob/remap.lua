31425c76e25be06501eaf69dd147d572ab3b17d7
Rob Bertram <rob@Robs-MacBook-Pro.local>
2025-09-03 04:34:58 -0400

    fix telescope

--- lua/rob/remap.lua
+++ lua/rob/remap.lua
@@ -35,14 +35,6 @@ vim.keymap.set("n", "<leader>ww", "<Cmd>BlameToggle window<CR>")
 vim.keymap.set("n", "<leader>wv", "<Cmd>BlameToggle virtual<CR>") -- close current
 
 
-local builtin = require('telescope.builtin')
-vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
-vim.keymap.set("n", "<leader>fw", builtin.live_grep, {})
-vim.keymap.set("n", "<leader>fb", builtin.buffers, {})
-vim.keymap.set("n", "<leader>fh", builtin.help_tags, {})
-vim.keymap.set("n", "<leader>fs", builtin.git_files, {})
-vim.keymap.set("n", "<leader>fo", builtin.oldfiles, {})
-
 vim.keymap.set("n", "q", "<Cmd>DiffviewClose<CR>")
 
 
